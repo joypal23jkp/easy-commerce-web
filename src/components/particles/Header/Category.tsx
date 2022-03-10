d5fa31dd8fe565dd85e1026967b7bbb8292dca35
joypa <joypal.23.jkp@gmail.com>
2022-03-11 00:48:27 +0600

    Header design updated.

---
+++ src/components/particles/Header/Category.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Category(): any {
+
+    return(
+        <div>
+            ok
+        </div>
+    );
+}
--- src/components/particles/Header/Header.tsx
+++ src/components/particles/Header/Header.tsx
@@ -14,7 +14,7 @@ import {
   HeartOutlined,
   ShoppingCartOutlined,
 } from "@ant-design/icons";
-// import Image1 from "../../../assets/images/Letter.png";
+import {Avatar, Button, Image} from "antd";
 
 export default function Header() {
   return (
@@ -110,17 +110,19 @@ export default function Header() {
             display: "flex",
           }}
         >
+
           <div className="nav_item" css={navItem}>
             <Link
-              to="/login"
+              to={"/"}
               style={{
                 color: "#5A5B6A",
                 fontWeight: 600,
               }}
             >
-              Sign in
+                <Button style={{
+                    borderRadius: 4
+                }}>Wishlist</Button>
             </Link>
-            <UserOutlined style={{ fontSize: "18px", padding: "4px" }} />
           </div>
           <div className="nav_item" css={navItem}>
             <Link
@@ -130,9 +132,10 @@ export default function Header() {
                 fontWeight: 600,
               }}
             >
-              Wishlist
+                <Button style={{
+                    borderRadius: 4
+                }}>Sign in</Button>
             </Link>
-            <HeartOutlined style={{ fontSize: "18px", padding: "4px" }} />
           </div>
           <div className="nav_item" css={navItem}>
             <Link
@@ -142,11 +145,25 @@ export default function Header() {
                 fontWeight: 600,
               }}
             >
-              Cart
+              <Button style={{
+                  borderRadius: 4
+              }}>
+                  My Cart
+              </Button>
             </Link>
-            <ShoppingCartOutlined
-              style={{ fontSize: "18px", padding: "4px" }}
-            />
+
+          </div>
+          <div className="nav_item" css={navItem}>
+            <Link
+              to={"/"}
+              style={{
+                color: "#5A5B6A",
+                fontWeight: 600,
+              }}
+            >
+                <Avatar src={<Image src="https://joeschmoe.io/api/v1/random" style={{ width: 32 }} />} />
+            </Link>
+
           </div>
         </nav>
       </div>
